3c23fc04cb1c51607378f2fe27362aeff7eca63e
root <root@alancamp6.mylabserver.com>
2017-10-23 07:33:00 -0400

    what?

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,5 +1,11 @@
 file 'default www' do
 	path '/var/www/html/index.html'
-	content 'Hello World!'
+	content 'Hello World! V1.0'
 end
+
+#webnodes = search('node', 'role:web')
+
+#webnodes.each do |node|
+	puts node
+#end
 
